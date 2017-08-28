75724550b6c301aa78ca7ff5197e8b66cc068f0a
Yannis Alexiou <yannisalexiou@users.noreply.github.com>
2017-08-28 16:57:08 +0300

    Methods thar are useful in Modern View

--- app/view/main/MainController.js
+++ app/view/main/MainController.js
@@ -5,5 +5,15 @@
 Ext.define('NastedJsonBind.view.main.MainController', {
     extend: 'Ext.app.ViewController',
 
-    alias: 'controller.main'
+    alias: 'controller.main',
+
+    onItemSelected: function (sender, record) {
+        Ext.Msg.confirm('Confirm', 'Are you sure?', 'onConfirm', this);
+    },
+
+    onConfirm: function (choice) {
+        if (choice === 'yes') {
+            //
+        }
+    }
 });
