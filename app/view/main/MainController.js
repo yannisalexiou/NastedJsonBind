e2d6a948286e071b888ac0e797867d1b1384149c
Yannis Alexiou <yannisalexiou@users.noreply.github.com>
2017-08-28 16:54:36 +0300

    Unused Method from Controller Deleted

--- app/view/main/MainController.js
+++ app/view/main/MainController.js
@@ -5,15 +5,5 @@
 Ext.define('NastedJsonBind.view.main.MainController', {
     extend: 'Ext.app.ViewController',
 
-    alias: 'controller.main',
-
-    onItemSelected: function (sender, record) {
-        Ext.Msg.confirm('Confirm', 'Are you sure?', 'onConfirm', this);
-    },
-
-    onConfirm: function (choice) {
-        if (choice === 'yes') {
-            //
-        }
-    }
+    alias: 'controller.main'
 });
